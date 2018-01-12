c51d43b76f63ff913cb6f162e45d73dcc2a053b6
Jordan McLean <jordan@trag.co>
2018-01-12 22:25:38 +0000

    Monzo API Integration

---
+++ plugins/monzo/controller.js
@@ -0,0 +1,71 @@
+function Monzo($scope, $http, $q, $interval) {
+
+    function updateAccounts() {
+        $scope.monzoAccounts = [];
+        var q = $q.defer();
+
+        const req = {
+            method: 'GET',
+            url: 'https://api.monzo.com/accounts',
+            headers: {
+                'Authorization': `Bearer ${config.monzo.token}`
+            }
+        }
+
+        $http(req).then((res) => {
+            $scope.monzoAccounts = res.data.accounts;
+            q.resolve();
+        });
+
+        return q.promise;
+    }
+
+    function updateBalances() {
+        $scope.monzoBalances = [];
+        $scope.monzoAccounts.forEach(account => {
+            const req = {
+                method: 'GET',
+                url: `https://api.monzo.com/balance?account_id=${account.id}`,
+                headers: {
+                    'Authorization': `Bearer ${config.monzo.token}`
+                }
+            }
+    
+            $http(req).then((res) => {
+                $scope.monzoBalances[account.id] = res.data;
+            });
+        });
+    }
+
+    function updatePots() {
+        $scope.monzoPots = [];
+        const req = {
+            method: 'GET',
+            url: `https://api.monzo.com/pots/listV1`,
+            headers: {
+                'Authorization': `Bearer ${config.monzo.token}`
+            }
+        }
+
+        $http(req).then((res) => {
+            $scope.monzoPots = res.data.pots;
+            console.log($scope.monzoPots);
+        });
+    }
+
+    function updateInterface() {
+        updateAccounts().then(() => {
+            updateBalances();
+            if (config.monzo.potsEnabled) {
+                updatePots();
+            }
+        });
+    }
+
+    updateInterface();
+    $interval(updateInterface, config.monzo.refreshTime * 1000);
+
+}
+
+angular.module('SmartMirror')
+    .controller('Monzo', Monzo);
